2beb61d87c87a9056fdaf9ea75bae2241b94c8a5
NoDioxide <104847051+NoDioxide@users.noreply.github.com>
2024-05-04 12:06:02 +0300

    Update header.php

--- blocks/header.php
+++ blocks/header.php
@@ -2,8 +2,8 @@
     <span class="logo">logo</span>
     <nav>
         <ul>
-            <li class="active"><a href="/">Home</a></li>
-            <li><a href="/about.php">About us</a></li>
+            <li class="active"><a href="/WebTest">Home</a></li>
+            <li><a href="/WebTest/about.php">About us</a></li>
             <li><a href="#">Portfolio</a></li>
             <li><a href="#">News</a></li>
             <li><a href="/reg.php">Registration</a></li>
